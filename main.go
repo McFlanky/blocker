dc0f7e3f40c6e59acf7d2a19054a0f6c2c81e9fe
McFlanky <cturjanica02@gmail.com>
2024-05-23 16:49:22 -0400

    Added mongoDB connection

--- main.go
+++ main.go
@@ -1,25 +1,53 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
 
 	"github.com/McFlanky/hotel-reservations-api/api"
+	"github.com/McFlanky/hotel-reservations-api/types"
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dburi = "mongodb://localhost:27017"
+const dbname = "hotel-reservation"
+const userColl = "users"
+
 func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	coll := client.Database(dbname).Collection(userColl)
+	user := types.User{
+		FirstName: "James",
+		LastName:  "Bond",
+	}
+	_, err = coll.InsertOne(ctx, user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var james types.User
+	if err := coll.FindOne(ctx, bson.M{}).Decode(&james); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(james)
+
 	listenAddr := flag.String("listenAddr", ":5000", "listen address to api server")
 	flag.Parse()
 
 	app := fiber.New()
 	apiv1 := app.Group("api/v1")
 
-	app.Get("/foo", handleFoo)
 	apiv1.Get("/user", api.HandleGetUsers)
 	apiv1.Get("/user/{id}", api.HandleGetUser)
 	app.Listen(*listenAddr)
-}
-
-func handleFoo(c *fiber.Ctx) error {
-	return c.JSON(map[string]string{"msg": "working just fine"})
 }
