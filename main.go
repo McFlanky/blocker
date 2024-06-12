4eda1bbb0758841ebe1437f2b6acafd013e598bf
McFlanky <cturjanica02@gmail.com>
2024-06-12 10:50:01 -0400

    Added database layer with mongodb and new fiber config error message whenever a new fiber is created

--- main.go
+++ main.go
@@ -3,13 +3,11 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"log"
 
 	"github.com/McFlanky/hotel-reservations-api/api"
-	"github.com/McFlanky/hotel-reservations-api/types"
+	"github.com/McFlanky/hotel-reservations-api/db"
 	"github.com/gofiber/fiber/v2"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -18,36 +16,27 @@ const dburi = "mongodb://localhost:27017"
 const dbname = "hotel-reservation"
 const userColl = "users"
 
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		return c.JSON(map[string]string{"error": err.Error()})
+	},
+}
+
 func main() {
+	listenAddr := flag.String("listenAddr", ":8000", "listen address to API server")
+	flag.Parse()
+
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dburi))
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx := context.Background()
-	coll := client.Database(dbname).Collection(userColl)
-	user := types.User{
-		FirstName: "James",
-		LastName:  "Bond",
-	}
-	_, err = coll.InsertOne(ctx, user)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// handler initialization
+	userHandler := api.NewUserHandler(db.NewMongoUserStore(client))
 
-	var james types.User
-	if err := coll.FindOne(ctx, bson.M{}).Decode(&james); err != nil {
-		log.Fatal(err)
-	}
-
-	fmt.Println(james)
-
-	listenAddr := flag.String("listenAddr", ":5000", "listen address to api server")
-	flag.Parse()
-
-	app := fiber.New()
+	app := fiber.New(config)
 	apiv1 := app.Group("api/v1")
 
-	apiv1.Get("/user", api.HandleGetUsers)
-	apiv1.Get("/user/{id}", api.HandleGetUser)
+	apiv1.Get("/user", userHandler.HandleGetUsers)
+	apiv1.Get("/user/:id", userHandler.HandleGetUser)
 	app.Listen(*listenAddr)
 }
