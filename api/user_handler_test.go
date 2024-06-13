ca268b7e52380a4fd1ee56dea4448c51873e265d
McFlanky <cturjanica02@gmail.com>
2024-06-13 14:14:37 -0400

    Added more in deptyh testing for testing to see if user and params fields are teh same in collection to dorp them by creating a new json decoder from the resp.body and decoding it to the user

--- api/user_handler_test.go
+++ api/user_handler_test.go
@@ -1,16 +1,24 @@
 package api
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"log"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/McFlanky/hotel-reservations-api/db"
+	"github.com/McFlanky/hotel-reservations-api/types"
+	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-const testdburi = "mongodb://localhost:27017"
+const (
+	testdburi = "mongodb://localhost:27017"
+	dbname    = "hotel-reservation-test"
+)
 
 type testdb struct {
 	db.UserStore
@@ -28,11 +36,41 @@ func setup(t *testing.T) *testdb {
 		log.Fatal(err)
 	}
 	return &testdb{
-		UserStore: db.NewMongoUserStore(client),
+		UserStore: db.NewMongoUserStore(client, dbname),
 	}
 }
 
 func TestPostUser(t *testing.T) {
 	tdb := setup(t)
 	defer tdb.teardown(t)
+
+	app := fiber.New()
+	userHandler := NewUserHandler(tdb.UserStore)
+	app.Post("/", userHandler.HandlePostUser)
+
+	params := types.CreateUserParams{
+		Email:     "some@foo.com",
+		FirstName: "foo",
+		LastName:  "bar",
+		Password:  "slgnslglsgljljnbl",
+	}
+	b, _ := json.Marshal(params)
+
+	req := httptest.NewRequest("POST", "/", bytes.NewReader(b))
+	req.Header.Add("Content-Type", "application/json")
+	resp, err := app.Test(req)
+	if err != nil {
+		t.Error(err)
+	}
+	var user types.User
+	json.NewDecoder(resp.Body).Decode(&user)
+	if user.FirstName != params.FirstName {
+		t.Errorf("expected username: %s but got: %s", params.FirstName, user.FirstName)
+	}
+	if user.LastName != params.LastName {
+		t.Errorf("expected lastname: %s but got: %s", params.LastName, user.LastName)
+	}
+	if user.Email != params.Email {
+		t.Errorf("expected email: %s but got: %s", params.Email, user.Email)
+	}
 }
