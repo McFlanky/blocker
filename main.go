e845744fc2ff6b5b43c26ebe2e563b2950d99016
McFlanky <cturjanica02@gmail.com>
2024-06-16 15:57:44 -0400

    Added booking adn room handlers for booking rooms that use booking struct/constructor in types.go + REMEMBER changed JWT auth function to take in a userstore and return a fiber handler so that we can decorate the returned func(c *fiber.Ctx) error {} to have access to JWT coupled with setting the user to the context of fiber so that we have it in every request

--- main.go
+++ main.go
@@ -20,6 +20,7 @@ var config = fiber.Config{
 }
 
 func main() {
+	// 2024-06-16 15:52:43.451410759 -0400 EDT m=+0.000754701
 	listenAddr := flag.String("listenAddr", ":8000", "listen address to API server")
 	flag.Parse()
 
@@ -40,9 +41,10 @@ func main() {
 		userHandler  = api.NewUserHandler(userStore)
 		hotelHandler = api.NewHotelHandler(store)
 		authHandler  = api.NewAuthHandler(userStore)
+		roomHandler  = api.NewRoomHandler(store)
 		app          = fiber.New(config)
 		auth         = app.Group("/api")
-		apiv1        = app.Group("/api/v1", middleware.JWTAuthentication)
+		apiv1        = app.Group("/api/v1", middleware.JWTAuthentication(userStore))
 	)
 
 	// auth handlers
@@ -61,5 +63,6 @@ func main() {
 	apiv1.Get("/hotel/:id", hotelHandler.HandleGetHotel)
 	apiv1.Get("/hotel/:id/rooms", hotelHandler.HandleGetRooms)
 
+	apiv1.Post("/room/:id/book", roomHandler.HandleBookRoom)
 	app.Listen(*listenAddr)
 }
